5daeedcf04765f0ab184afe91428abc66a7d2cf7
lovecashmere <bruce@lovecashmere.com>
2020-06-14 22:04:22 +0100

    Add files via upload

--- projects/FlashLoan/utils/Owned.sol
+++ projects/FlashLoan/utils/Owned.sol
@@ -15,3 +15,12 @@ contract Owned {
             _;
         }
 }
+
+contract Destroyable is Owned {
+
+    // Withdraw funds to the owner before destroying the contract!
+    function destroyContract() public OwnerOnly {
+        Owner.transfer(address(this).balance);
+        selfdestruct(Owner);
+    }
+}
