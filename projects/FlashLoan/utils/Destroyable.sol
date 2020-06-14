41eb2d9b8417679f7ab27191a3a2f0f321b45bd2
lovecashmere <bruce@lovecashmere.com>
2020-06-14 21:57:08 +0100

    Add files via upload

--- projects/FlashLoan/utils/Destroyable.sol
+++ projects/FlashLoan/utils/Destroyable.sol
@@ -2,15 +2,15 @@ pragma solidity ^0.6.6;
 
 import "https://github.com/lovecashmere/crypto/projects/FlashLoan/utils/Owned.sol";
 
-contract Destroyable is Owned {
+contract Destroyable {
 
-    modifier () {
+    modifier OwnerOnly {
           require(msg.sender == Owner, "Only the owner can call this function.");
           _;
     }
 
     // Withdraw funds to the owner before destroying the contract!
-    function destroyContract() public {
+    function destroyContract() public OwnerOnly {
         Owner.transfer(address(this).balance);
         selfdestruct(Owner);
     }
