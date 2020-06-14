106fc22173d6bd047f4e4a3f6199575f194a5f6d
lovecashmere <bruce@lovecashmere.com>
2020-06-14 22:02:39 +0100

    Add files via upload

--- projects/FlashLoan/utils/Owned.sol
+++ projects/FlashLoan/utils/Owned.sol
@@ -2,13 +2,16 @@ pragma solidity ^0.6.6;
 
 contract Owned {
 
+    address payable public Owner;
+
     constructor() public {
-        address payable public Owner;
+
         Owner = msg.sender;
 
-        modifier OwnerOnly {
+    }
+
+    modifier OwnerOnly {
             require(msg.sender == Owner, "Only the owner can call this function.");
             _;
         }
-    }
 }
