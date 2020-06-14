6d9a161b641dc0cf9a9d64c295059808d678b8f6
lovecashmere <bruce@lovecashmere.com>
2020-06-14 22:26:09 +0100

    Add files via upload

--- projects/FlashLoan/utils/Destroyable.sol
+++ projects/FlashLoan/utils/Destroyable.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Destroyable is Owned {
+contract Destroyable is Ownable{
 
     // Withdraw funds to the owner before destroying the contract!
     function destroyContract() public onlyOwner {
